ce2d1ea117c15e6bb1c5eff84aa76d0a81927f75
emirhanzengindev <emiir1230@gmail.com>
2026-01-13 20:28:27 +0300

    Fix next.config.js to use serverComponentsExternalPackages instead of serverExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  serverExternalPackages: ['bcryptjs'],
   reactStrictMode: true,
   swcMinify: true,
   experimental: {
-    serverComponentsExternalPackages: ['@supabase/supabase-js'],
+    serverComponentsExternalPackages: ['@supabase/supabase-js', 'bcryptjs'],
   },
 };
