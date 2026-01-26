0fb59e859aa6d9bbbb2e720c8691e3ad1c7878d3
emirhanzengindev <emiir1230@gmail.com>
2026-01-26 11:53:23 +0300

    Move protected routes from (protected) to (app) group to resolve duplicate path error in Next.js 16.1.4

--- src/app/(protected)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import MainLayout from "@/components/MainLayout";
-
-export default function ProtectedLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <MainLayout>{children}</MainLayout>;
-}
